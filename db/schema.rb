eaaa2993f9775ddcbe138c0ccf82e115b160962e
satoshi.takeo <pastation3rd@gmail.com>
2018-05-28 09:08:42 +0000

    DRY

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180526215340) do
+ActiveRecord::Schema.define(version: 20180528084850) do
 
   create_table "tasks", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "status"
   end
 
 end
